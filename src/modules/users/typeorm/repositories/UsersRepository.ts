29e6839a56d39722648721545c5053e5e974f317
estevesryan <ryanplay71@gmail.com>
2023-10-04 16:26:33 -0300

    criando repositorios dos usuarios

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,41 @@
+import { EntityRepository, Repository } from 'typeorm';
+import User from '../enitities/User';
+
+@EntityRepository(User)
+
+// busca um ususario pelo Nome
+class UserRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return user;
+  }
+
+  // busca um usuario pelo id
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
+  // busca um usuario pelo email
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+}
+
+export default UserRepository;
